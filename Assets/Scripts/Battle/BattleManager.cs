785f3f8f1be0fed03f7a4099d3e3960f87c3a396
Derek Mayer <deverex1317@gmail.com>
2014-11-03 19:55:32 -0600

    Initial commit with Battle Manager, Player, and Enemy objects.

---
+++ Assets/Scripts/Battle/BattleManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using BattleObjects;
+
+public class BattleManager : MonoBehaviour {
+
+	Player gamePlayer = new Player();
+	Enemy gameEnemy = new Enemy();
+
+	// Use this for initialization
+	void Start() {
+	
+	} // end Start()
+	
+	// Update is called once per frame
+	void Update() {
+	
+	} // end Update()
+} // end class
