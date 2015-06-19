67d4512d6d9e9be4b7da59c7b692c207bac40cd2
Derek Mayer <deverex1317@gmail.com>
2015-06-19 10:16:00 -0500

    Forgot to add some files...

---
+++ Assets/Scripts/Battle/BaseChampion.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Abilities;
+
+public class BaseChampion {
+	public string name;
+	public string description;
+	public float health;
+	public float strength;
+	public float armor;
+	public float accuracy;
+	public float evasion;
+	public float critChance;
+	public float critMultiplier;
+	public float blockChance;
+	public float blockMultiplier;
+	public float finesse;
+	public float attackSpeed;
+	public float armorPen;
+	public float dexterity;
+	public float tenacity;
+	public float resolve;
+	public float basicAttackCooldown;
+
+	public string target;
+
+	public List<Ability> abilities = new List<Ability>();
+}
---
+++ Assets/Scripts/Battle/TestHero.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Abilities;
+
+public class TestHero : BaseChampion {
+
+	public TestHero() {
+        name = "Test";
+		description = "I like butts.";
+		health = 1000;
+		strength = 100;
+		armor = 100;
+		accuracy = 50;
+		evasion = 50;
+		critChance = 20;
+		critMultiplier = 200;
+		blockChance = 20;
+		blockMultiplier = 50;
+		finesse = 30;
+		attackSpeed = 5;
+		armorPen = 0;
+		dexterity = 5;
+		tenacity = 0;
+		resolve = 0;
+		basicAttackCooldown = 3;
+
+		target = "";
+	} // end constructor
+}
